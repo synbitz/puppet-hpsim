d8d252d3780dbcebf735e57cd00c310266fb6048
cegeka-jenkins <cegeka-jenkins@cegeka.be>
2012-10-29 08:26:17 +0100

    Initial repository layout for puppet module hpsim

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: hpsim
+#
+# This module manages hpsim
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+class hpsim {
+
+
+}
---
+++ spec/classes/hpsim_spec.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env rspec
+
+require 'spec_helper'
+
+describe 'hpsim' do
+  it { should contain_class 'hpsim' }
+end
